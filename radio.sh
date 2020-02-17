a5ccb7bb0ca097517c8f35d00b3bec31b19596bc
devel@mauriciocap.com <devel@mauriciocap.com>
2020-02-17 00:52:47 -0300

    OK: extrae autor y todos los mensajes, parsea

--- radio.sh
+++ radio.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
+export PATH=$PATH:~/bin
+export DISPLAY=:0.0 #A: para el chrome/puppeteer
 TS=`date +%Y%m%d` #A: aÃ±o mes dia
 AUDIO=data_radio/audio/$TS
 rm audio*.ogg
-#echo "whatsapp_pa_radio.js" | node main.js #A: bajo los audios
+echo "whatsapp_pa_radio.js" | node main.js #A: bajo los audios
 (cd data_radio; pagit pull)
 mkdir -p $AUDIO
 mv audio* $AUDIO
--- whatsapp_pa_radio.js
+++ whatsapp_pa_radio.js
@@ -1,6 +1,12 @@
 //INFO: bajar audios desde la ultima marca
-MARCA='El robot de PodemosAprender guardo hasta aca';
+GrpName= global.GrpName || 'PodemosAprender radio';
+MARCA='El robot de PodemosAprender guardó hasta acá';
 stop= false;
+
+function fname_safe(s) {
+	return (s||'_').replace(/[^A-Za-z0-9_-]+/g,'_');
+}
+
 (async () => {
 	if (global.radioRunning) { return } radioRunning= true;
 
@@ -9,10 +15,10 @@ stop= false;
 		await Whatsapp_page.goto('https://web.whatsapp.com/');
 	}
 
-	await Whatsapp_page.waitForXPath('//*[text()="PodemosAprender radio"]',{timeout:120000}); //A: esperamos hasta 2min
+	await Whatsapp_page.waitForXPath('//*[text()="'+GrpName+'"]',{timeout:120000}); //A: esperamos hasta 2min
 
 	var isGroupSelected= false;
-	var xb= await Whatsapp_page.$x('//*[text()="PodemosAprender radio"]');
+	var xb= await Whatsapp_page.$x('//*[text()="'+GrpName+'"]');
 	for (var i=0; i<xb.length; xb++) {
 		var up3= await up(xb[i],3);
 		L("Elegir grupo "+i+" "+up3);
@@ -45,25 +51,48 @@ stop= false;
 		//TODO: da timeout 30s await Whatsapp_page.waitForNavigation({waitUntil: 'networkidle0'});
 		await sleep(10000); //A: esperar 5s que cargue! 
 		if (x.length>0) { break; } //A: subi hasta que la encontre, listo otro paso
-		console.log("Buscando ...");
+		console.log("Buscando marca ...");
 		Whatsapp_page.keyboard.press('Home'); //A: no la encontre, apreto "home" para subir un poco
 	}
+	//A: llegue a la ultima marca
 
-	var audios= await Whatsapp_page.$x('//*[text()="'+MARCA+'"]/following::audio')
-	//A: consigo los audios DESPUES de la marca, usando xpath <3 <3 <3
-	console.log("Audios len=" + audios.length);
-	await Promise.all(audios.map(async (a,i) => {
-		var url= await a.getProperty("src");
-		console.log("Audio URL: "+ url);
-		return downloadUrl(Whatsapp_page,sprintf('audio%02d.ogg',i),url); 
+	var msgEs= await Whatsapp_page.$x("//*[text()='"+MARCA+"']/following::*[contains(concat(' ',normalize-space(@class),' '),' message-')]");		
+	var msgt= await Whatsapp_page.evaluate( (... msgEs) => {
+		return msgEs.map(e => e.innerHTML);
+	}, ... msgEs);
+	console.log("MSG: "+msgt.length);
+	fs.writeFileSync('xmsg',JSON.stringify(msgt,null,1),'utf-8');
+
+	var whoPrev='unknown';
+	msg= msgt.map(t => {
+		var r= {};
+		r.m= t.match(/>[^<]+</g).map(c => c.replace(/[><]/g,''));
+		var a= t.match(/<audio .*?src="([^"]*)/);	
+		if (a) { r.a= a[1];}
+		if (t.match(/message-out/)) { r.who="MauricioCap"; }
+		else if (r.m.length>2) { r.who= r.m[1]; }
+		else { r.who= whoPrev; }
+		whoPrev= r.who;
+		return r;
+	});
+
+	await Promise.all(msg.map(async (m,i) => {
+		if (m.a) {
+			var url= m.a;
+			var fname= 'audio'+sprintf('%02d',i)+'_'+fname_safe(m.who)+'.ogg';
+			console.log("Audio URL: "+i+' '+fname+' '+url);
+			return downloadUrl(Whatsapp_page,fname,url); 
+		}
 	}));
 
 	console.log("done");
-	if (! global.dontExit) { process.exit(); }
-	return ;
 
+	if (true) {
 	await Whatsapp_page.keyboard.type(MARCA);
 	await Whatsapp_page.keyboard.press('Enter');
 	await Whatsapp_page.keyboard.type('Para la fecha '+(new Date().toISOString()));
 	await Whatsapp_page.keyboard.press('Enter');
+	}
+
+	if (! global.dontExit) { await Browser.close(); process.exit(); }
 })();

fe317d9d5810101be599e918e25c675fc4bdd8ac
devel@mauriciocap.com <devel@mauriciocap.com>
2020-02-25 10:17:24 -0300

    OK: formato nuevo

--- mkRadio.sh
+++ mkRadio.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 
-(cd $1; if [ -f _normalized ] ; then echo "Normalized before"; else ffmpeg-normalize *.ogg -c:a libopus -ext ogg; mv normalized/* .; rmdir normalized; touch _normalized; git add *; fi)
-echo "normalized"
+IN=$1
+OUT=$2
+mkdir -p $OUT
 
-(cd data_radio; for i in `find -iname *.ogg | sort` ; do echo "<p>$i<br><audio controls><source src=\"$i\" type=\"audio/ogg\"></audio></p>" ; done > programas.html ; git add programas.html ; git commit -m "actualizado $1" ; pagit push)
-#actualice el indice en la web
-
-rm x_audio.txt
-for f in data_radio/audio/c_in.ogg $1/audio*.ogg data_radio/audio/c_out.ogg; do 
-	if ffmpeg -i $f 2>&1 | grep Stream | grep -q opus; then 
-		echo "OPUS $f"
-		echo "file '$f'" >> x_audio.txt; 
+for f in $IN/*.ogg ; do 
+	of=$OUT/`basename $f`
+	if [ -f $of ] ; then
+		echo "HAVE $f" 
 	else
-		fok=`dirname $f`/x_`basename $f`
-		ffmpeg -y -i $f -c:a libopus $fok
-		echo "file '$fok'" >> x_audio.txt; 
-	fi
+		echo "NORMALIZE $f" 
+		ffmpeg-normalize $f -c:a libopus -ext ogg -o $of
+	fi	
 done
 
-ffmpeg -y -f concat -safe 0 -i x_audio.txt -c copy x_audio.ogg
-#A: tengo un solo audio
-
-#ffmpeg -y -loop 1 -i data_radio/img/portada.jpg -i x_audio.ogg -c:v libx264 -tune stillimage -c:a aac -b:a 64k -pix_fmt yuv420p -shortest x_radio.mp4 #A: video, por ahora no usamos
+cp -u $IN/*.msg.json $OUT/
+(cd $OUT ; for f in *.msg.json ; do echo $f >> index_${f:0:6}.txt ; echo ${f:0:6} >> index.txt; done)
 
